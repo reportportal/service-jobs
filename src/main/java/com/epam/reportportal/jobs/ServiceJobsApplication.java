eb57b582172d75e68d68c41d7197cf1697e4de68
Pavel_Bortnik <pavel_bortnik@epam.com>
2021-03-10 13:45:29 +0300

    Rename package

--- src/main/java/com/epam/reportportal/jobs/ServiceJobsApplication.java
+++ src/main/java/com/epam/reportportal/jobs/ServiceJobsApplication.java
@@ -1,4 +1,4 @@
-package com.epam.reportportal.servicejobs;
+package com.epam.reportportal.jobs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
