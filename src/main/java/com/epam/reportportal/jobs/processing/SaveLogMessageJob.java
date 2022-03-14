8dbe3bbf0812ea1ad2f9cc2e1509b683bedb2a2d
Maksim Antonov <Maksim_Antonov@epam.com>
2022-03-14 11:10:58 +0200

    Clean code

--- src/main/java/com/epam/reportportal/jobs/processing/SaveLogMessageJob.java
+++ src/main/java/com/epam/reportportal/jobs/processing/SaveLogMessageJob.java
@@ -2,10 +2,8 @@ package com.epam.reportportal.jobs.processing;
 
 import com.epam.reportportal.elastic.dao.LogMessageRepository;
 import com.epam.reportportal.log.LogMessage;
-import com.epam.reportportal.processing.SaveLogMessageProcessing;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.scheduling.concurrent.DefaultManagedTaskScheduler;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
@@ -13,21 +11,16 @@ import java.util.Objects;
 @Service
 public class SaveLogMessageJob {
     public static final String LOG_MESSAGE_SAVING_QUEUE_NAME = "log_message_saving";
-
-//    private final SaveLogMessageProcessing saveLogMessageProcessing;
     private final LogMessageRepository logMessageRepository;
 
     public SaveLogMessageJob(LogMessageRepository logMessageRepository) {
         this.logMessageRepository = logMessageRepository;
-//        saveLogMessageProcessing = new SaveLogMessageProcessing(logMessageRepository,100, 3000,
-//                new DefaultManagedTaskScheduler());
     }
 
     @RabbitListener(queues = LOG_MESSAGE_SAVING_QUEUE_NAME, containerFactory = "processingRabbitListenerContainerFactory")
     public void execute(@Payload LogMessage logMessage) {
         if (Objects.nonNull(logMessage)) {
             logMessageRepository.save(logMessage);
-//            saveLogMessageProcessing.add(logMessage);
         }
     }
 }
