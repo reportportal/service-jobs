16c09aa953f76c9349a65fe6777382fa3910d66c
Andrei Piankouski <andrei_piankouski@epam.com>
2025-01-17 11:25:56 +0300

    EPMRPP-90461 || Add separate from previous line.

--- src/main/java/com/epam/reportportal/config/IAMCredentialSupplier.java
+++ src/main/java/com/epam/reportportal/config/IAMCredentialSupplier.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.epam.reportportal.config;
 
 import com.google.common.base.Supplier;
