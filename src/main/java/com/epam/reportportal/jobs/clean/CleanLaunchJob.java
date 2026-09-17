7d8125bd41ddb1144ccb87d09672fd443d1dea46
UU <36200539+imashkanov@users.noreply.github.com>
2026-09-17 12:12:21 +0300

    EPMRPP-121575: [TMS][WS] Core jobs clean Manual Launches which were created > Keep launches project setting

--- src/main/java/com/epam/reportportal/jobs/clean/CleanLaunchJob.java
+++ src/main/java/com/epam/reportportal/jobs/clean/CleanLaunchJob.java
@@ -28,7 +28,7 @@ public class CleanLaunchJob extends BaseCleanJob {
   private static final String START_TIME_PARAM = "startTime";
   private static final String SELECT_LAUNCH_ID_QUERY =
       "SELECT id FROM launch WHERE project_id = :projectId AND start_time <= "
-          + ":startTime::TIMESTAMP AND retention_policy = 'REGULAR'";
+          + ":startTime::TIMESTAMP AND retention_policy = 'REGULAR' AND launch_type <> 'MANUAL'";
   private static final String DELETE_CLUSTER_QUERY =
       "DELETE FROM clusters WHERE clusters.launch_id IN (:ids);";
   private static final String DELETE_LAUNCH_QUERY = "DELETE FROM launch WHERE id IN (:ids);";
